7a13002ab74b55eac2092ebcec894fd762482a76
masum <mclasix@gmail.com>
2025-01-01 03:37:41 +0700

    [FEATURE] -> Add Movie Detail

--- Movies/Utilities/APIClient.swift
+++ Movies/Utilities/APIClient.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+class APIClient {
+    private let baseURL: String = "https://api.themoviedb.org/3"
+    private let apiKey: String = "ab8ca7862f2733af2356c592de5f9d45"
+
+    func getApiKey() -> String {
+        return apiKey
+    }
+    
+    func getBaseUrl() -> String {
+        return baseURL
+    }
+}
