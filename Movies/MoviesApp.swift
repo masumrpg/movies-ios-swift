719ff38250ccdbf038bf25a960527e9a679b9c8a
masum <mclasix@gmail.com>
2024-12-31 13:31:42 +0700

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Ma'sum on 31/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
